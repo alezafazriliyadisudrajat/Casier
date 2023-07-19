6e4554598c12a6f9750f710995232f71dc5d51ba
alezafazriliyadisudrajat <alezafazriliyadisudrajat@smkwikrama.sch.id>
2023-07-19 09:00:57 +0700

    Casier Sederhana

---
+++ src/casier/Casier.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package casier;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Casier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new kasir().show();
+    }
+    
+}
